a0e6e8e5fbffb481c158f19181c10de6e58a8a13
Mci61 <melihcemipek@hotmail.com>
2021-12-26 13:12:19 +0300

    sistem-ozellikleri calismasi eklendi.

---
+++ sistem_ozellikleri.py
@@ -0,0 +1,20 @@
+import platform
+
+# İşletim sistemi
+print(platform.system())
+
+# İşlemci bilgisi
+print(platform.processor())
+
+# Nesil bilgisi
+print(platform.release())
+
+# PC Bağlantı adı
+print(platform.node())
+
+# Python Versiyonu
+print(platform.python_version())
+
+# Python Compiler Bilgisi
+print(platform.python_compiler())
+
